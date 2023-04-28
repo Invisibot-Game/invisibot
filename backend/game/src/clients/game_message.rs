b129bc56d471ddf2dacdb76d3b7627dad6e0ed53
Vidar Magnusson <vidar.magnusson@cygni.se>
2023-04-29 00:16:24 +0200

    Update GameRound type

--- backend/game/src/clients/game_message.rs
+++ backend/game/src/clients/game_message.rs
@@ -1,6 +1,12 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
-use crate::{game_logic::game_state::GameState, player::PlayerId};
+use crate::{
+    game_logic::{game_map::TileType, game_state::GameState},
+    player::PlayerId,
+    utils::coordinate::Coordinate,
+};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", content = "payload")]
@@ -16,10 +22,7 @@ impl GameMessage {
     }
 
     pub fn game_round(game_state: GameState, player_id: PlayerId) -> GameMessage {
-        Self::GameRound(GameRound {
-            game_state,
-            player_id,
-        })
+        Self::GameRound(GameRound::new(&game_state, &player_id))
     }
 
     pub fn goodbye(message: String) -> GameMessage {
@@ -38,6 +41,32 @@ impl GameMessage {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GameRound {
-    game_state: GameState,
-    player_id: PlayerId,
+    pub width: u32,
+    pub height: u32,
+    pub walls: Vec<Coordinate>,
+    pub visible_players: HashMap<PlayerId, Coordinate>,
+    pub own_player_id: PlayerId,
+}
+
+impl GameRound {
+    pub fn new(game_state: &GameState, current_player: &PlayerId) -> Self {
+        Self {
+            width: game_state.map.width,
+            height: game_state.map.height,
+            walls: game_state
+                .map
+                .tiles
+                .iter()
+                .filter(|t| t.tile_type == TileType::Wall)
+                .map(|t| t.coord.clone())
+                .collect(),
+            visible_players: game_state
+                .players
+                .iter()
+                .filter(|&(id, p)| id == current_player || p.is_visible())
+                .map(|(id, p)| (id.clone(), p.get_pos().clone()))
+                .collect(),
+            own_player_id: current_player.clone(),
+        }
+    }
 }
