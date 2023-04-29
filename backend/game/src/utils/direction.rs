ac255cd85702c3599ce8add0f022a3514da81c69
Vidar Magnusson <vidar.magnusson@cygni.se>
2023-04-29 10:57:19 +0200

    Derive everything for dir

--- backend/game/src/utils/direction.rs
+++ backend/game/src/utils/direction.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub enum Direction {
     Up,
     Down,
