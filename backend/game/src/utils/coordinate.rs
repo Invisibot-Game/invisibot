50bac118ecacfcfdc698abf8ccd9ae4a08b7ca24
Vidar Magnusson <vidar.magnusson@cygni.se>
2023-04-29 10:31:38 +0200

    Add translate util method on coordinate

--- backend/game/src/utils/coordinate.rs
+++ backend/game/src/utils/coordinate.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use super::direction::Direction;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Coordinate {
     pub x: u32,
@@ -11,4 +13,15 @@ macro_rules! coord {
     ( $x:expr, $y:expr ) => {{
         Coordinate { x: $x, y: $y }
     }};
+}
+
+impl Coordinate {
+    pub fn translate(&self, dir: &Direction) -> Self {
+        match dir {
+            Direction::Up => coord!(self.x, self.y - 1),
+            Direction::Down => coord!(self.x, self.y + 1),
+            Direction::Right => coord!(self.x + 1, self.y),
+            Direction::Left => coord!(self.x - 1, self.y),
+        }
+    }
 }
