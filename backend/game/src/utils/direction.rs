5f6f2a2488a8d4c65e2e2905b82d63749bfc3998
Vidar Magnusson <vidar.magnusson@cygni.se>
2023-04-29 11:11:00 +0200

    Add opposite function to dir

--- backend/game/src/utils/direction.rs
+++ backend/game/src/utils/direction.rs
@@ -17,4 +17,13 @@ impl Direction {
             Direction::Left,
         ]
     }
+
+    pub fn opposite(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Right => Direction::Left,
+            Direction::Left => Direction::Right,
+        }
+    }
 }
