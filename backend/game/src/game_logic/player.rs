9ce95edbe190ad0d6f2cc737624fe60b91a21cec
Vidar Magnusson <git@vidarmagnusson.com>
2023-05-02 22:16:36 +0200

    Add rotation

--- backend/game/src/game_logic/player.rs
+++ backend/game/src/game_logic/player.rs
@@ -1,17 +1,26 @@
 use serde::{Deserialize, Serialize};
 
-use crate::{clients::player_id::PlayerId, utils::coordinate::Coordinate};
+use crate::{
+    clients::player_id::PlayerId,
+    utils::{coordinate::Coordinate, direction::Direction},
+};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Player {
     id: PlayerId,
     pos: Coordinate,
+    rotation: Direction,
     visible: bool,
 }
 
 impl Player {
     pub fn new(id: PlayerId, pos: Coordinate, visible: bool) -> Self {
-        Self { id, pos, visible }
+        Self {
+            id,
+            pos,
+            rotation: Direction::Up,
+            visible,
+        }
     }
 
     pub fn get_id(&self) -> &PlayerId {
@@ -27,9 +36,12 @@ impl Player {
     }
 
     pub fn update_pos(player: &Player, new_pos: Coordinate, visible: bool) -> Self {
+        let rotation =
+            Coordinate::dir_between(&player.pos, &new_pos).unwrap_or(player.rotation.clone());
         Self {
             id: player.id.clone(),
             pos: new_pos,
+            rotation,
             visible,
         }
     }
@@ -38,6 +50,7 @@ impl Player {
         Self {
             id: player.id.clone(),
             pos: player.pos.clone(),
+            rotation: player.rotation.clone(),
             visible,
         }
     }
--- backend/game/src/utils/coordinate.rs
+++ backend/game/src/utils/coordinate.rs
@@ -30,4 +30,148 @@ impl Coordinate {
             Direction::Left => coord!(self.x - 1, self.y),
         }
     }
+
+    /// Returns the direction necessary for going from `from` to `to`.
+    /// Returns None if `from` == `to` or if the movement was purely diagonal.
+    pub fn dir_between(from: &Coordinate, to: &Coordinate) -> Option<Direction> {
+        let delta_x = (from.x as i32) - (to.x as i32);
+        let delta_y = (from.y as i32) - (to.y as i32);
+        let delta_x_abs = delta_x.abs();
+        let delta_y_abs = delta_y.abs();
+
+        Some(match (delta_x, delta_y) {
+            (0, 0) => return None,
+            (_, y) if delta_y_abs > delta_x_abs && y < 0 => Direction::Down,
+            (_, y) if delta_y_abs > delta_x_abs && y > 0 => Direction::Up,
+            (x, _) if delta_x_abs > delta_y_abs && x < 0 => Direction::Right,
+            (x, _) if delta_x_abs > delta_y_abs && x > 0 => Direction::Left,
+            (_, _) => return None,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::{coordinate::Coordinate, direction::Direction};
+
+    #[test]
+    fn going_one_step_down_returns_down() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(1, 1), &coord!(1, 2)),
+            Some(Direction::Down)
+        );
+    }
+
+    #[test]
+    fn going_one_step_right_returns_right() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(1, 1), &coord!(2, 1)),
+            Some(Direction::Right)
+        );
+    }
+
+    #[test]
+    fn going_one_step_up_returns_up() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(1, 1), &coord!(1, 0)),
+            Some(Direction::Up)
+        );
+    }
+
+    #[test]
+    fn going_one_step_left_returns_left() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(1, 1), &coord!(0, 1)),
+            Some(Direction::Left)
+        );
+    }
+
+    #[test]
+    fn more_up_than_right_returns_up() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(2, 0)),
+            Some(Direction::Up)
+        )
+    }
+
+    #[test]
+    fn more_up_than_left_returns_up() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(6, 0)),
+            Some(Direction::Up)
+        )
+    }
+
+    #[test]
+    fn more_down_than_right_returns_down() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(2, 8)),
+            Some(Direction::Down)
+        )
+    }
+
+    #[test]
+    fn more_down_than_left_returns_down() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(6, 8)),
+            Some(Direction::Down)
+        )
+    }
+
+    #[test]
+    fn more_right_than_up_returns_right() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(8, 2)),
+            Some(Direction::Right)
+        )
+    }
+
+    #[test]
+    fn more_right_than_down_returns_right() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(8, 6)),
+            Some(Direction::Right)
+        )
+    }
+
+    #[test]
+    fn more_left_than_up_returns_left() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(0, 2)),
+            Some(Direction::Left)
+        )
+    }
+
+    #[test]
+    fn more_left_than_down_returns_left() {
+        assert_eq!(
+            Coordinate::dir_between(&coord!(4, 4), &coord!(0, 6)),
+            Some(Direction::Left)
+        )
+    }
+
+    #[test]
+    fn equal_coords_returns_none() {
+        assert_eq!(Coordinate::dir_between(&coord!(4, 4), &coord!(4, 4)), None)
+    }
+
+    #[test]
+    fn diagonal_up_right_returns_none() {
+        assert_eq!(Coordinate::dir_between(&coord!(4, 4), &coord!(6, 2)), None)
+    }
+
+    #[test]
+    fn diagonal_down_right_returns_none() {
+        assert_eq!(Coordinate::dir_between(&coord!(4, 4), &coord!(6, 6)), None)
+    }
+
+    #[test]
+    fn diagonal_up_left_returns_none() {
+        assert_eq!(Coordinate::dir_between(&coord!(4, 4), &coord!(2, 2)), None)
+    }
+
+    #[test]
+    fn diagonal_down_left_returns_none() {
+        assert_eq!(Coordinate::dir_between(&coord!(4, 4), &coord!(2, 6)), None)
+    }
 }
