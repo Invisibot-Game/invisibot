e3045ea6837e47b2d40265fc936f76c68126451b
Vidar Magnusson <git@vidarmagnusson.com>
2023-05-06 00:51:26 +0200

    Add tests for direction methods

--- backend/game/src/utils/direction.rs
+++ backend/game/src/utils/direction.rs
@@ -34,3 +34,46 @@ impl Direction {
         }
     }
 }
+
+#[cfg(test)]
+mod all_dirs_tests {
+    use super::Direction;
+
+    #[test]
+    fn all_dirs_returns_expected() {
+        let all_dirs = Direction::all_dirs();
+        assert_eq!(all_dirs[0], Direction::Up);
+        assert_eq!(all_dirs[1], Direction::Right);
+        assert_eq!(all_dirs[2], Direction::Down);
+        assert_eq!(all_dirs[3], Direction::Left);
+    }
+}
+
+#[cfg(test)]
+mod opposite_tests {
+    use super::Direction;
+
+    #[test]
+    fn opposite_up_is_down() {
+        let up = Direction::Up;
+        assert_eq!(up.opposite(), Direction::Down)
+    }
+
+    #[test]
+    fn opposite_down_is_up() {
+        let down = Direction::Down;
+        assert_eq!(down.opposite(), Direction::Up)
+    }
+
+    #[test]
+    fn opposite_right_is_left() {
+        let right = Direction::Right;
+        assert_eq!(right.opposite(), Direction::Left)
+    }
+
+    #[test]
+    fn opposite_left_is_right() {
+        let left = Direction::Left;
+        assert_eq!(left.opposite(), Direction::Right)
+    }
+}
